68f5d1509956775a912b917a9ff266e7531a4dfd
EisaAlnawafleh <eisaalnawafleh802@gmail.com>
2025-12-25 17:44:16 +0300

    size

--- app/codex/edit/page.jsx
+++ app/codex/edit/page.jsx
@@ -52,23 +52,23 @@ function EditContent() {
   }, [incomingCode]);
 
   return (
-    <div className="h-screen flex flex-col bg-[#0b0b10]">
-      <div className="h-12 flex items-center px-6 border-b border-white/10 text-white">
-        <span className="font-semibold">Codex Editor</span>
+    <div className="h-screen flex flex-col  bg-[#0b0b10]   ">
+      <div className="h-12  flex codex_e  items-center px-6 border-b border-white/10  text-white">
+        <span className="font-semibold ">Codex Editor</span>
       </div>
 
-      <div className="flex flex-1">
+      <div className="flex w-full h-full edit_codex ">
         <div className="w-1/2 relative">
-          <div className="absolute top-3 right-5 z-50 flex gap-5">
+          <div className="absolute top-3 right-15 z-50 flex gap-5">
             <Link
               href="/codex"
-              className="bg-red-700 hover:scale-105 transition-all duration-700 rounded text-white px-5 py-1"
+              className="bg-red-700  whitespace-nowrap  edit_b hover:scale-105 transition-all duration-700 rounded text-white px-[3%] py-1"
             >
               Back Codex
             </Link>
             <button
               onClick={() => setResult(code)}
-              className="bg-green-600 hover:scale-105 transition-all duration-700 text-white px-5 py-1 rounded opacity-90 hover:opacity-100"
+              className="bg-green-600 edit_b_1 whitespace-nowrap hover:scale-105 transition-all duration-700 text-white px-[5%] py-1 rounded opacity-90 hover:opacity-100"
             >
               Run ▶
             </button>
--- app/components/header_nav.jsx
+++ app/components/header_nav.jsx
@@ -3,8 +3,17 @@ import { useState, useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Nav = () => {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setOpen(false);
+  }, [pathname]);
+  const linkClass = (path) =>
+    pathname === path ? "bg-[#1b1c1d] rounded-xl" : "";
+
   const [open, setOpen] = useState(false);
 
   useEffect(() => {
@@ -18,14 +27,11 @@ const Nav = () => {
 
   return (
     <>
-      {/* ===== Navbar Bar (NOT fixed, NOT h-screen) ===== */}
       <nav className="  fixed w-full  px-2 top-4  text-white flex items-center justify-between   z-50">
-        {/* Logo */}
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-[#7220e4] to-[#f55cfe] font-bold">
           DTAL.COM
         </div>
 
-        {/* Burger Button */}
         <button
           onClick={() => setOpen(!open)}
           className={`
@@ -35,7 +41,9 @@ const Nav = () => {
     flex flex-col items-center justify-center
     gap-2 sm:gap-2.5
     transition-transform duration-500
-    ${open ? "rotate-180" : ""}
+    ${open ? "rotate-180 " : ""}
+    
+    
   `}
         >
           <span
@@ -43,7 +51,7 @@ const Nav = () => {
       block rounded bg-purple-400
       h-[3px] sm:h-1
       transition-all duration-500
-      ${open ? "absolute top-1/2 w-full rotate-45 -translate-y-1/2" : "w-[70%]"}
+      ${open ? "" : "w-[70%]"}
     `}
           />
           <span
@@ -59,66 +67,67 @@ const Nav = () => {
       block rounded bg-purple-400
       h-[3px] sm:h-1
       transition-all duration-500
-      ${
-        open ? "absolute top-1/2 w-full -rotate-45 -translate-y-1/2" : "w-[70%]"
-      }
+      ${open ? "" : "w-[70%]"}
     `}
           />
         </button>
       </nav>
 
-      {/* ===== Overlay ===== */}
-      {open && (
-        <div
-          onClick={() => setOpen(false)}
-          className="fixed inset-0 bg-black/60 z-[9998]"
-        />
-      )}
+      <div
+        onClick={() => setOpen(false)}
+        className={`
+    fixed inset-0 bg-black/60 z-[9998]
+    transition-opacity duration-900
+    ${open ? "opacity-100" : "opacity-0 pointer-events-none"}
+  `}
+      />
 
-      {/* ===== Side Menu ===== */}
       {open && (
         <div
           data-aos="fade-right"
-          className="
-            fixed top-0 left-0
-            h-screen w-[80%] max-w-[300px]
-            bg-[#111] text-white
-            p-4 shadow-2xl
-            z-[9999]
-            rounded-r-xl
-          "
+          className={`
+    fixed top-0 left-0
+    h-screen w-[80%] max-w-[300px]
+    bg-[#111] text-white
+    p-4 shadow-2xl
+    z-[9999]
+    rounded-r-xl
+
+    transition-all duration-500 ease-in-out
+    ${open ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0"}
+  `}
         >
           <Link href="/">
             <div className="text-transparent px-3 py-2  bg-clip-text bg-gradient-to-r from-[#7220e4] to-[#f55cfe] font-bold">
               DTAL.COM
             </div>
           </Link>
-<div className={`flex flex-col justify-between h-full`}>
-
-
-          <div className="flex flex-col  gap-3 pt-6">
-            <Link href="/">
-              <div className="bg-[#1b1c1d] px-3 py-2 rounded-xl">Home</div>
-            </Link>
-            <Link href="/codex">
-              <div className="hover:text-purple-400 px-3 py-2">Codex</div>
-            </Link>
-            <Link href="/upload">
-              <div className="hover:text-purple-400 px-3 py-2">
-                Video to text
-              </div>
-            </Link>
-            <Link href="/pdf">
-              <div className="hover:text-purple-400 px-3 py-2">
-                Speech To PDF
-              </div>
-            </Link>
-          </div>
-          <Link href="/auth">
-            <div className=" mb-[20%]  justify-center items-center flex flex-col flex-1  gap-2 border w-[80%] text-center py-1 rounded-[5px] cursor-pointer transition-all duration-300]">
-              Get Started
+          <div className={`flex flex-col gap-[5%] h-full`}>
+            <div className="flex flex-col  gap-3 pt-6">
+              <Link href="/" className={linkClass("/")}>
+                <div className=" focus:bg-[#1b1c1d] focus:rounded-xl px-3 py-2 ">
+                  Home
+                </div>
+              </Link>
+              <Link href="/codex" className={linkClass("/codex")}>
+                <div className="hover:text-purple-400 px-3 py-2">Codex</div>
+              </Link>
+              <Link href="/upload" className={linkClass("/upload")}>
+                <div className="hover:text-purple-400 px-3 py-2">
+                  Video to text
+                </div>
+              </Link>
+              <Link href="/pdf" className={linkClass("/pdf")}>
+                <div className="hover:text-purple-400 px-3 py-2">
+                  Speech To PDF
+                </div>
+              </Link>
             </div>
-          </Link>
+            <Link href="/auth" className={linkClass("/auth")}>
+              <div className="   justify-center items-center flex flex-col flex-1  gap-2 border w-[80%] text-center py-1 rounded-[5px] cursor-pointer transition-all duration-300]">
+                Get Started
+              </div>
+            </Link>
           </div>
         </div>
       )}
